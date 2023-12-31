98b3a23dee9fdceff26171261c87ce7339cca5a0
KamilMarzynski <kamil.marzynski@gmail.com>
2023-12-31 12:44:29 +0100

    Refactor POST and PUT functions in server.ts

--- src/routes/api/posts/[id]/+server.ts
+++ src/routes/api/posts/[id]/+server.ts
@@ -3,12 +3,9 @@ import { posts, validPostId } from '$lib/server';
 
 
 export async function PUT({ request, params }) {
-	const { content, title, image } = await request.json();
+	const { content, title } = await request.json();
     const { id } = params;
 
-    console.log('content', content)
-    console.log('title', title)
-    console.log('image', image)
 
     if (!validPostId(id)) {
         return json({ error: 'Invalid post id' }, { status: 400 });
@@ -17,7 +14,6 @@ export async function PUT({ request, params }) {
     const post = await posts.update(id, {
         content,
         title,
-        // image
     });
 
 	return json({ id: post.id }, { status: 200 });
