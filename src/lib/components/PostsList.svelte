10cea806cd73534409fb22b57e1b6d6e7cc26619
KamilMarzynski <kamil.marzynski@gmail.com>
2024-01-01 19:04:13 +0100

    Remove commented out style code in PostsList.svelte

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -13,15 +13,3 @@
 		<ListPostItem {id} {title} {image} />
 	{/each}
 </div>
-
-<!-- <style>
-	ul {
-		list-style: none;
-		margin: 0;
-		padding: 0;
-	}
-
-	li {
-		margin-bottom: 1rem;
-	} -->
-<!-- </style> -->
